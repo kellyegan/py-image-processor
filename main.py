d82e12843c729c4571ce8f1b9963a69c6d245ddf
Kelly Egan <kelly.egan@gmail.com>
2022-08-19 08:20:25 -0400

    Add kivy

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("py-image-processor")
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class ImageProcessor(Widget):
+    pass
+
+
+class ImageProcessorApp(App):
+    def build(self):
+        return ImageProcessor()
+
+
+if __name__ == '__main__':
+    ImageProcessorApp().run()
