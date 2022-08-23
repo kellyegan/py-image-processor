6100492e9ca1568b7e66c74a6d0d7bf8bb6b4fe7
Kelly Egan <kelly.egan@gmail.com>
2022-08-22 21:50:33 -0400

    Remove Kivy (for now).

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-
-
-class ImageProcessor(Widget):
-    pass
-
-
-class ImageProcessorApp(App):
-    def build(self):
-        return ImageProcessor()
-
 
 if __name__ == '__main__':
-    ImageProcessorApp().run()
+    print("Hello")
